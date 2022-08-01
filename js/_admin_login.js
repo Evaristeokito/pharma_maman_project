ec975239678f48f5c41e4bbad702750496cc3de7
Evaristeokito <mbukaokito40@gmail.com>
2022-08-01 17:03:47 +0200

    configuraton de la page login

--- js/_admin_login.js
+++ js/_admin_login.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+    $(".loading-spinner").hide();
+
     $('#admin_login_form').on('submit', function(event) {
         event.preventDefault();
         $.ajax({
@@ -9,6 +11,7 @@ $(document).ready(function() {
             dataType: "json",
             beforeSend: function() {
                 $('#admin_login').val('validation...');
+                $(".loading-spinner").show();
                 $('#admin_login').attr('disabled', 'disabled');
             },
             success: function(data) {
@@ -17,18 +20,21 @@ $(document).ready(function() {
                 }
                 if (data.error) {
                     $('#admin_login').val('connexion');
+                    setTimeout(() => {
+                        $(".loading-spinner").hide()
+                    }, 4000);
                     $('#admin_login').attr('disabled', false);
 
                     iziToast.show({
-                        title: "Notification",
+                        title: "Erreur :",
                         titleColor: "white",
-                        message: "Le nom administrateur ou mot de passe invalid",
+                        message: "Le nom administrateur ou Mot de passe incorrect",
                         messageColor: "white",
-                        backgroundColor: "#009688",
+                        backgroundColor: "#ba000d",
                         timeout: 4000,
                         position: "topRight",
                         progressBar: true,
-                        progressBarColor: "#4db6ac",
+                        progressBarColor: "#f44336",
                     });
                 }
             }
--- views/login.php
+++ views/login.php
@@ -1,12 +1,10 @@
 <?php 
 include "components/head.php";
-
 ?>
 
 <?php 
   // Login PHP
   include __DIR__ . "../../action/connection_database.php";
- 
  
   if (isset($_SESSION['user_id']))
   {
@@ -18,14 +16,10 @@ include "components/head.php";
 <div class="login-page">
  <div class="login-box">
   <div class="login-logo">
+    <img src="../public/assets/user-img/logo-pharma.jpg" alt="IMG" style="width: 250px;">
   </div>
   <!-- /.login-logo -->
   <div class="card">
-    <div class="card-header">
-      <div class="login-logo">
-        <img src="../public/assets/user-img/lagos.jpg" width="90" height="90" alt="IMG">
-     </div>
-    </div>
     <div class="card-body login-card-body">
       <p class="login-box-msg">Admnistration Pharmacie</p>
 
@@ -33,7 +27,7 @@ include "components/head.php";
        <span id="error_message" class="text text-danger"></span>
 
         <div class="input-group mb-3">
-          <input type="text" class="form-control text text-capitalize" id="admin_user_name" name="admin_user_name" placeholder="Nom utilisateur">
+          <input type="text" class="form-control text text-capitalize" id="admin_user_name" name="admin_user_name" placeholder="utilisateur">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-user"></span>
@@ -53,22 +47,19 @@ include "components/head.php";
         </div>
 
         <div class="row">
-          <div class="col-7">
-            <div class="icheck-primary">
-              <!-- <input type="checkbox" id="remember">
-              <label for="remember">
-               souvenir de moi
-              </label> -->
-            </div>
-          </div>
           <!-- /.col -->
-          <div class="col-5">
-            <input type="submit" id="admin_login" name="admin_login" class="btn btn-primary btn-block" value="Connexion">
+          <div class="col-12 mt-4">
+            <input type="submit" id="admin_login" name="admin_login" class="btn btn-success btn-block" value="Connexion">
           </div>
           <!-- /.col -->
         </div>
 
       </form>
+      <!-- loading page -->
+      <div class="loading-spinner"><div class="auto-loading">
+        <div></div><div><div></div></div>
+      </div></div>
+      <!-- end loading page -->
     </div>
     <!-- /.login-card-body -->
   </div>
